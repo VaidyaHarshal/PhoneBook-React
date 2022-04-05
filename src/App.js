e563459a5dc60dda3d3d3ad6dc2a855842a6db71
Harshal Vaidya <92479500+VaidyaHarshal@users.noreply.github.com>
2022-04-04 23:56:52 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,97 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [entries, setEntries] = useState([]);
+
+  const addEntryToPhoneBook = (entry) => {
+    setEntries([...entries, entry]);
+  };
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        flexDirection: "column",
+      }}
+    >
+      <EntryForm addEntryToPhoneBook={addEntryToPhoneBook} />
+      <DisplayEntries entries={entries} />
+    </div>
+  );
+}
+
+function EntryForm({ addEntryToPhoneBook }) {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Inside onSubmit");
+    addEntryToPhoneBook({ firstName, lastName, phoneNumber });
+  };
+  return (
+    <form
+      onSubmit={handleSubmit}
+      style={{ display: "flex", flexDirection: "column" }}
+    >
+      <input
+        type="text"
+        name="firstName"
+        id="firstName"
+        placeholder="First Name"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+      />
+      <input
+        type="text"
+        name="lastName"
+        id="lastName"
+        placeholder="Last Name"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+      />
+      <input
+        type="text"
+        name="phoneNumber"
+        id="phoneNumber"
+        placeholder="Phone Number"
+        value={phoneNumber}
+        onChange={(e) => setPhoneNumber(e.target.value)}
+      />
+      <button type="submit" onSubmit={handleSubmit}>
+        Submit
+      </button>
+    </form>
+  );
+}
+
+function DisplayEntries({ entries }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Phone Number</th>
+        </tr>
+      </thead>
+      <tbody>
+        {entries.map((entry) => {
+          return (
+            <tr key={`${entry.firstName} ${entry.lastName}`}>
+              <td>{entry.firstName}</td>
+              <td>{entry.lastName}</td>
+              <td>{entry.phoneNumber}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+export default App;
